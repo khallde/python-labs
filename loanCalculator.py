c32aa80783a7fcfdbbe2aa20c4ae47301d6195ee
Katie Hall <me@katiehall.de>
2020-05-20 16:45:33 -0700

    commit

--- loanCalculator.py
+++ loanCalculator.py
@@ -1,10 +1,10 @@
-# Prompting user input.
-# Remember that the interest rate is a percentage.
+# Prompting user input
 loanAmount = float(input("Enter the cost of the loan: "))
 interestRate = float(input("Enter the interest rate: "))
 numberOfYears = int(input("Enter the number of years for the loan: "))
 
-# How do I get this to display only two decimal points?
+# Calculating the monthly payment
 monthlyPayment = loanAmount * (interestRate * (1 + interestRate) * numberOfYears) / ((1 + interestRate) * numberOfYears - 1)
 
+# Printing a message to the user
 print("Your loan payment is ${0:.2f} a month for {1:d} years.".format(monthlyPayment, numberOfYears))
