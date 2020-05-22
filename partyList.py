45b920ea072bdb2794534aa2ce7bcd787463ce26
Katie Hall <me@katiehall.de>
2020-05-21 18:02:26 -0700

    Set the script to prompt the use less, made the output more readable to the end user.

--- partyList.py
+++ partyList.py
@@ -1,11 +1,19 @@
 nameList = []
 name = ""
+attemptCount = False
 
 while name != "Stop":
-    name = input("Enter names. Enter Stop to stop. ").capitalize()
+    if attemptCount == False:
+        name = input("Enter names. Enter Stop to stop. ").capitalize()
+        attemptCount = True
+    else:
+        name = input("").capitalize()
     nameList.append(name)
 
 nameList.remove("Stop")
 nameList.sort()
 
-print(nameList)
+print("The names entered were: ")
+
+for listItem in nameList:
+    print(listItem)
